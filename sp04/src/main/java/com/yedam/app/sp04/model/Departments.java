eb31ca632f116107ae1ccf589b23105a52dd8d62
882Eunae <pubao47@gmail.com>
2025-05-27 16:55:20 +0900

    sss

---
+++ sp04/src/main/java/com/yedam/app/sp04/model/Departments.java
@@ -0,0 +1,11 @@
+package com.yedam.app.sp04.model;
+
+import lombok.Data;
+
+@Data
+public class Departments {
+	private String departmentId; 
+	private String departmentName;
+	private String managerId; 
+	private String locationId;
+}
